4b52da1342c47d880118a537bb2736ed7e63a5c2
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-05-23 14:00:32 +0200

    updated install_requires in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
 install_requires = [
+    "Cartopy>=0.17.0",
+    "descartes>=1.1.0",
+    "geopandas>=0.4.1",
+    "netCDF4==1.5.0.1",
     "numpy>=1.16.2",
+    "matplotlib>=3.0.3",
     "pandas>=0.24.2",
     "rasterio>=1.0.22",
+    "requests>=2.21.0",
+    "sentinelsat>=0.13",
+    "scipy>=1.2.1",
+    "Shapely>=1.6.4",
 ]
 
 
